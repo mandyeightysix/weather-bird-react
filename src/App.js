7f27ec640c3de9fdb9f1594eccc51cad491d352e
mandyeightysix <96144054+mandyeightysix@users.noreply.github.com>
2023-04-11 08:50:57 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <footer>
         This project was coded by Amanda Pepin and is{" "}
-        <a href="https://github.com/mandyeightysix/weather-bird-react" target="_blank">
+        <a href="https://github.com/mandyeightysix/weather-bird-react" target="_blank" rel="noreferrer">
           open-sourced on GitHub
         </a>
         </footer>
