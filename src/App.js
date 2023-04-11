bc8e0114b55534069c2eff86a91d6bc949da93ab
mandyeightysix <96144054+mandyeightysix@users.noreply.github.com>
2023-04-11 08:27:49 -0400

    Included footer and github links

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <footer>
+        This project was coded by Amanda Pepin and is{" "}
+        <a href="https://github.com/mandyeightysix/weather-bird-react" target="_blank">
+          open-sourced on GitHub
+        </a>
+        </footer>
     </div>
   );
 }
